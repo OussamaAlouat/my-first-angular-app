3fa2585e8be05f1d29388ee448fa310bea1f9f87
oussamaAlouat <al317257@uji.es>
2020-01-10 15:34:24 +0100

    Get params from route url

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'product-details',
   templateUrl: 'product-details.component.html'
 })
 
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit{
   public title: string;
+  public product;
 
-  constructor() {
+  constructor(
+    private route:ActivatedRoute,
+  ) {
     this.title = 'Product details';
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = params.get('productId');
+    })
   }
 }
